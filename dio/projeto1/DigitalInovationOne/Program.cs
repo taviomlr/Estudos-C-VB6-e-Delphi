cd42b16df754c220e5fa9083308b3bd877d4d925
taviomlr <taviorodrigues@gmail.com>
2022-01-20 20:11:20 -0300

    Exercícios do curso Primeiros Passos co .NET

---
+++ dio/projeto1/DigitalInovationOne/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DigitalInovationOne
+{
+  
+  class Program
+  {
+    
+    static void Main(string[] args)
+    {
+      int numeroDeVezes = 10;
+
+      for (int i = 0; i < numeroDeVezes; i++)
+      {
+        Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
+      }
+    }
+  }
+}
